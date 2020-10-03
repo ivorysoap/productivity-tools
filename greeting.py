a43091bb0ae264b5badd6dccfd19d33d32682401
Ivor <ivor.benderavage@gmail.com>
2020-10-03 19:42:08 -0400

    Code refactor

--- greeting.py
+++ greeting.py
@@ -2,17 +2,27 @@ import os
 import requests
 from datetime import datetime
 
+def getCommitsToday(username, today = None):
+    if today == None:
+        today = datetime.today().strftime('%Y-%m-%d')
+        
+    responses = requests.get('https://api.github.com/users/{}/events'.format(username))
+    commits = 0
+    for response in responses.json():
+        if(response['type'] == 'PushEvent' and response['created_at'][:10] == today):
+            commits = commits + 1
+    
+    return commits
+        
+
 # Remove later.
 username = 'ivorysoap'
 
 today = datetime.today().strftime('%Y-%m-%d')
 
 os.system('echo "Welcome, $(whoami)!  [$(date)]"')
-responses = requests.get('https://api.github.com/users/{}/events'.format(username))
-commits = 0
-for response in responses.json():
-	if(response['type'] == 'PushEvent' and response['created_at'][:10] == today):
-		commits = commits + 1
+
+commits = getCommitsToday(username)
 
 if commits == 0:
 	output = 'echo "You have not yet committed anything today."'
