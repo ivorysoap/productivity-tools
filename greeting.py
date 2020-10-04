c86f93eaa7dac846d6ec3fe14685abfad5ad50d5
Ivor <ivor.benderavage@gmail.com>
2020-10-03 22:58:53 -0400

    Added greeting.py

---
+++ greeting.py
@@ -0,0 +1,26 @@
+import os
+import requests
+from datetime import datetime
+
+# Remove later.
+username = 'ivorysoap'
+
+today = datetime.today().strftime('%Y-%m-%d')
+
+os.system('echo "Welcome, $(whoami)!  [$(date)]"')
+responses = requests.get('https://api.github.com/users/{}/events'.format(username))
+commits = 0
+for response in responses.json():
+	if(response['type'] == 'PushEvent' and response['created_at'][:10] == today):
+		commits = commits + 1
+
+if commits == 0:
+	output = "You have not yet committed anything today."
+elif commits == 1:
+	output = "You have committed once today.  Nice job!"
+elif commits > 1:
+	output = "You have committed {} times today.  Nice job!"
+
+os.system('echo output')
+exit()
+
