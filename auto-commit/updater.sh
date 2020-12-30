f64aa35a2d431c75f6b823b596009ee0625baec9
Ivor <ivor.benderavage@gmail.com>
2020-12-29 23:57:47 -0500

    Update updater.sh

--- auto-commit/updater.sh
+++ auto-commit/updater.sh
@@ -6,9 +6,11 @@ if test -f "$FILE"; then
 else
 	echo "$FILE does not exist"
 	touch file
+fi
 
 echo $((1 + $RANDOM % 1000000)) >> file
 git add file
 git commit -m "Scheduled commit"
 git push
 
+exit 1
